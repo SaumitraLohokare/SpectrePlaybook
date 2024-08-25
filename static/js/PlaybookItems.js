3471bec30a27702cdd2143c9478ad32734ec042b
SaumitraLohokare <saumitra.lohokare@gmail.com>
2024-08-25 10:09:26 -0700

    Added functionality to delete items from playbook layer

--- static/js/PlaybookItems.js
+++ static/js/PlaybookItems.js
@@ -1,23 +1,7 @@
 const PADDING = 8;
 export const DEFAULT_ICON_SIZE = 24;
 
-function drawRoundedRect(context, x, y, width, height, radius, color) {
-    context.beginPath();
-    context.moveTo(x + radius, y);
-    context.lineTo(x + width - radius, y);
-    context.arc(x + width - radius, y + radius, radius, 1.5 * Math.PI, 2 * Math.PI);
-    context.lineTo(x + width, y + height - radius);
-    context.arc(x + width - radius, y + height - radius, radius, 0, 0.5 * Math.PI);
-    context.lineTo(x + radius, y + height);
-    context.arc(x + radius, y + height - radius, radius, 0.5 * Math.PI, Math.PI);
-    context.lineTo(x, y + radius);
-    context.arc(x + radius, y + radius, radius, Math.PI, 1.5 * Math.PI);
-    context.closePath();
-
-    // Fill or stroke the path
-    context.fillStyle = color; // Change color as needed
-    context.fill();
-}
+import { drawRoundedRect } from "./drawUtils.js";
 
 function calculateAngle(originX, originY, x, y) {
     // Calculate the differences in coordinates
